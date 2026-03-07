acbd3a7cc020e4fc1b005df1d25b657f1b12b572
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-03-07 08:18:22 +0530

    remove hardcode values

--- app/router/chatbot.py
+++ app/router/chatbot.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter, Depends, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.helper.databaseConnection import get_db
 from app.schema.chatbotSchema import (HeaderDetail,SessionRequest, HeaderDetailOnlyUser)
+from app.helper.config import (
+    LAST_N_MESSAGES,
+    NO_OF_ROW_SUMMARY
+)
 from app.helper.helper import (
     get_header_without_session_details, 
     get_header_with_session_details
@@ -39,8 +43,8 @@ async def new_chat_session(sessionData: SessionRequest, getHeaderDetail:HeaderDe
 
 @router.post("/")
 async def chatbot(background_tasks: BackgroundTasks,message: str, getHeaderDetail: HeaderDetail = Depends(get_header_with_session_details), db: AsyncSession = Depends(get_db)):
-    last_n_messages = 30
-    noOfRow = 3
+    last_n_messages = LAST_N_MESSAGES
+    noOfRow = NO_OF_ROW_SUMMARY
     listofMessages = await chatHistoryList(last_n_messages,getHeaderDetail["sessionId"], db)
     summariazationMessage = await listofSummariazationMessages(noOfRow, getHeaderDetail["sessionId"], db)
     sementicSearchResult = await sementicSearch(message, getHeaderDetail["sessionId"],getHeaderDetail["userId"],db)
